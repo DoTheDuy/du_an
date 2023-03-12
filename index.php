d9399f744bd57e0c185631da93ce40e04525288c
kimtheanh <kimtheanh1208@gmail.com>
2023-03-12 08:17:22 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
+    session_start();
+
     require "view/header.php";
 
-    require "view/shop-details.php";
+    if(isset($_GET['act'])) {
+
+        $act = $_GET['act'];
+
+        require "view/navbar.php";
+
+        switch ($act) {
+            case "shop-details":
+                require "view/shop-details.php";
+                break;
+            default:
+                require "view/home.php";
+                break;
+        }
+    } else {
+        require "view/navbar.php";
+        require "view/home.php";
+    }
 
     require "view/footer.php";
-    hihii `23123213` 123
+?>
